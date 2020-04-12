3cd7ab2e11654e98d9f61a54d591b6ce8fe5692b
Youzi <xurong-m@egnev.com>
2020-04-12 22:21:51 +0800

    更新用户信息的云函数

---
+++ cloudfunctions/update-user-info/index.js
@@ -0,0 +1,38 @@
+/*
+ * @Description: 更新用户信息表的操作
+ * @Author: youzi
+ * @Date: 2020-04-11 18:40:34
+ * @LastEditors: youzi
+ * @LastEditTime: 2020-04-12 22:05:21
+ */
+// 初始化 cloud
+const cloud = require('wx-server-sdk');
+cloud.init({
+  // API 调用都保持和云函数当前所在环境一致
+  env: cloud.DYNAMIC_CURRENT_ENV
+});
+// 使用了 async await 语法
+const db = cloud.database();
+const _ = db.command;
+
+exports.main = async (event, context) => {
+  try {
+    console.log(event);
+    const wxContext = cloud.getWXContext();
+    // debugger
+    return await db
+      .collection('user_info')
+      .where({
+        _openid: wxContext.OPENID
+      })
+      // .get();
+      .update({
+        data: {
+          nickName: event.nickName,
+          wxid: event.wxid
+        }
+      });
+  } catch (e) {
+    console.error(e);
+  }
+};
