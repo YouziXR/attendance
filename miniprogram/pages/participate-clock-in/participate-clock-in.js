a86d48338e151b474ebc3c08b3028e575d7f392f
Youzi <youzi_xr@sina.com>
2020-04-21 19:56:10 +0800

    feat(pages): 参与打卡页，没做完，只测试了云函数本地调用，没有通过小程序调用

--- miniprogram/pages/participate-clock-in/participate-clock-in.js
+++ miniprogram/pages/participate-clock-in/participate-clock-in.js
@@ -4,56 +4,60 @@ Page({
    * 页面的初始数据
    */
   data: {
-    avatarUrl:
-      'https://wx.qlogo.cn/mmopen/vi_32/reRVI0fwEMx4A3AgOAegicXlib635ic0v71HKCShUldKvkgMQdDibzlHicXicMTk2T8P23ibd3ko6Cia51Pg8sII18sA1g/132',
-    title: '1',
-    timeStart: null,
-    status: true
+    activityInfo: {}
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {},
+  onLoad: function(options) {},
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {},
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
+  onReady: function() {
+    const d = new Date().getTime();
     this.setData({
-      timeStart: new Date()
+      activityInfo: {
+        url:
+          'https://wx.qlogo.cn/mmopen/vi_32/reRVI0fwEMx4A3AgOAegicXlib635ic0v71HKCShUldKvkgMQdDibzlHicXicMTk2T8P23ibd3ko6Cia51Pg8sII18sA1g/132',
+        title: '1',
+        startTime: d,
+        endTime: new Date(d + 60 * 60 * 1000).getTime(),
+        status: true
+      }
     });
   },
 
   /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {},
+
+  /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {},
+  onHide: function() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {},
+  onUnload: function() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {},
+  onPullDownRefresh: function() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function (e) {
+  onReachBottom: function(e) {
     console.warn(e, 'on reaching btn');
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {}
+  onShareAppMessage: function() {}
 });
