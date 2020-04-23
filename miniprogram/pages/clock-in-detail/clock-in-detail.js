adb0ba9302caebad057534f5cb1b30206aeccd41
Youzi <youzi_xr@sina.com>
2020-04-23 20:43:19 +0800

    fix(pages): 刚刚新建detail页面时新建成组件了

--- miniprogram/pages/clock-in-detail/clock-in-detail.js
+++ miniprogram/pages/clock-in-detail/clock-in-detail.js
@@ -1,23 +1,59 @@
-// pages/clock-in-detail/clock-in-detail.js
-Component({
+// miniprogram/pages/clock-in-detail/clock-in-detail.js
+Page({
   /**
-   * 组件的属性列表
-   */
-  properties: {
-
-  },
-
-  /**
-   * 组件的初始数据
+   * 页面的初始数据
    */
   data: {
-
+    activityInfo: {}
   },
 
   /**
-   * 组件的方法列表
+   * 生命周期函数--监听页面加载
    */
-  methods: {
+  onLoad: function (options) {
+    console.log(options.query);
+    const eventChannel = this.getOpenerEventChannel();
+    // 监听getInfoFromListPage事件，获取上一页面通过eventChannel传送到当前页面的数据
+    let that = this;
+    eventChannel.on('getInfoFromListPage', function (data) {
+      that.setData({
+        activityInfo: data
+      });
+    });
+  },
 
-  }
-})
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {},
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {},
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {},
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {},
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {},
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {}
+});
