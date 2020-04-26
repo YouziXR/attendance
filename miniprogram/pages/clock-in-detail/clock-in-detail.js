994e1172e4b2b335623aa16d75c8ccf34e8f32e7
Youzi <youzi_xr@sina.com>
2020-04-26 20:52:02 +0800

    perf(clock-in-detail): 完善一下打卡详情页面，还没完全搞定，等做了其他的再来完善

--- miniprogram/pages/clock-in-detail/clock-in-detail.js
+++ miniprogram/pages/clock-in-detail/clock-in-detail.js
@@ -6,7 +6,8 @@ Page({
    */
   data: {
     activityInfo: {},
-    createTime: ''
+    createTime: '',
+    startTime: ''
   },
 
   /**
@@ -17,11 +18,13 @@ Page({
     // 监听getInfoFromListPage事件，获取上一页面通过eventChannel传送到当前页面的数据
     let that = this;
     eventChannel.on('getInfoFromListPage', function (data) {
-      let { createTime } = data;
-      let date = dateFormat(createTime);
+      let { createTime, activityTime } = data;
+      let createDate = dateFormat(createTime);
+      let startDate = dateFormat(activityTime.startTime);
       that.setData({
         activityInfo: data,
-        createTime: `${date.year}${date.month}${date.date} ${date.day} ${date.hour}:${date.minute}`
+        createTime: `${createDate.year}${createDate.month}${createDate.date} ${createDate.day} ${createDate.hour}:${createDate.minute}`,
+        startTime: `${startDate.year}${startDate.month}${startDate.date} ${startDate.day} ${startDate.hour}:${startDate.minute}`
       });
     });
   },
