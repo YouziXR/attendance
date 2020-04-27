0ac6033c8b40d5572340ec11f45752146be3fa24
Youzi <youzi_xr@sina.com>
2020-04-27 19:24:45 +0800

    feat(): 依赖库更新到最新版本

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -1,9 +1,9 @@
 //app.js
 App({
   onLaunch: function (options) {
-    console.log(options)
+    // console.log(options)
     if (!wx.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力')
+      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
     } else {
       wx.cloud.init({
         // env 参数说明：
@@ -11,10 +11,10 @@ App({
         //   此处请填入环境 ID, 环境 ID 可打开云控制台查看
         //   如不填则使用默认环境（第一个创建的环境）
         env: 'mp-cloud-dev-test-ry755',
-        traceUser: true,
-      })
+        traceUser: true
+      });
     }
 
-    this.globalData = {}
+    this.globalData = {};
   }
-})
+});
